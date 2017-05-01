3b0186ebbf98dde84fb7a6c2345ee326a1fb2c61
Brian K <karabinchak.brian@gmail.com>
2017-05-01 17:02:45 -0400

    Searching adds a buton, clicking initial buttons work, rating shows up

--- assets/javascript/engine.js
+++ assets/javascript/engine.js
@@ -1,5 +1,65 @@
 //initial topics listed on site
 var topics = ['Arsenal FC', 'Barcelona FC', 'Funny Cats', 'Funny Dogs', 'Penguins', 'Steve Carell', 'American Dad' ];
+var results;
+//beta public API key
+var apiKey = 'dc6zaTOxFJmzC';
+
+//appending API response to DOM
+function appendGifToDOM(info) {
+	console.log('running appendGifToDom');
+	//creating Div to hold gif
+	var gifDiv = $('<div class = \'gif-container\'>');
+	var gifRating = $('<p>');
+	gifRating.append('Rating : '+info.rating);
+	var gifImage = $('<img class=\'gif\'>');
+	//make inital image the still image
+	gifImage.attr('src', info.images.downsized_still.url);
+	gifImage.attr('data-still', info.images.downsized_still.url);
+	gifImage.attr('data-animate', info.images.downsized.url);
+	gifImage.attr('data-state', 'still');
+	//append div with rating, class still, class giphy-gif
+	gifDiv.append(gifRating);
+	gifDiv.append(gifImage);
+	$('#gifDiv').append(gifDiv);
+	//and img src of moving gifs etc, all info i need
+};
+
+//create queryURL
+function createQueryURL(query) {
+	return 'http://api.giphy.com/v1/gifs/search?q=' + query + '&api_key='+ apiKey + '+&limit=10';
+}
+
+//clear gifDiv in Dom, in prep for next search
+function clearGifDOM(){
+	$('#gifDiv').html('');
+};
+
+//create initial buttons on site
+function populateButtons(){
+	for(var i=0; i<topics.length;i++) {
+		createButton(topics[i]);
+	}
+};
+
+//sending GET request to API
+function requestFromAPI(queryUrl) {
+	console.log('running request from API');
+	//since we'll be displaying stuff, gotta get rid of old stuff first
+	clearGifDOM();
+	$.ajax({
+		url: queryUrl,
+		method: 'GET'
+	}).done(function(response) {
+		results=response;
+		console.log('response:');
+		console.log(response);
+
+		for(var j=0; j<10; j++) {
+			appendGifToDOM(response.data[j]);
+		};
+
+	});
+};
 
 //function to add buttons in topic-button div
 function createButton(value) {
@@ -8,26 +68,41 @@ function createButton(value) {
 	$('#topicButtons').append(btn);
 }
 
-//create initial buttons on site
+//initalize DOM
 $(document).ready(function(){
-	for(var i=0; i<topics.length;i++) {
-		createButton(topics[i]);
-	}
-});
+	//populate premade buttons
+	populateButtons();
 
-$('#search-btn').on('click', function() {
-	//create button and addend it to topic-butons
+	//when Search Button Clicked...
+	$('#search-btn').click( function() {	
+		console.log('clicking search button');
+		//create button and addend it to topic-butons
+		var searchQuery = $('#search').val().trim();
+		//send API GET method, display results in DOM
+		requestFromAPI(createQueryURL(searchQuery));
+		//create a button in button row
+		createButton(searchQuery);
+	});
 
-	//send search to api
+	//if a topic button is clicked
+	$('.topic').on('click', function() {
+		//retrieve value of button		
+		var valueOfButton = $(this).val();
+		//send it to the api, display results
+		requestFromAPI(createQueryURL(valueOfButton));
+	});
 
-	//display results
-});
-
-//if a topic button is clicked
-$('.topic').on('click', function() {
-	//retrieve value of button
-
-	//send it to the api
-
-	//display results
+	$('.gif').on('click', function() {
+		console.log('Clicked a gif');
+		var state = $(this).attr('data-state');
+    	var animate = $(this).attr('data-animate');
+    	var still = $(this).attr('data-still');
+    	if(state==='still') {
+        	$(this).attr('data-state', 'animate');
+        	$(this).attr('src', animate);
+      	} else if (state!='still') {
+        	$(this).attr('data-state', 'still');
+        	$(this).attr('src', still);
+      	}
+	});
 });
